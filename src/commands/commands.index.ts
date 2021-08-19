3aeee3c4e1812dd15a914b00b5867cf9f59a3f36
Malo Polese <malo.polese@gmail.com>
2021-08-19 14:31:37 +0200

    fix: copy path bug if multiple key with same name

--- src/commands/commands.index.ts
+++ src/commands/commands.index.ts
@@ -1,17 +1,12 @@
 import { commands, Disposable, window } from 'vscode';
-import { EXTENTION_NAME } from '../constants';
 import { LoggerService } from '../logger.service';
 import { Copy } from './copy.command';
-
-const setCmdName = (name: string): string => {
-  return `${EXTENTION_NAME}.${name.toLowerCase()}`;
-};
 
 export const registerCommands = (): Disposable[] => {
   const loggerService = new LoggerService();
 
   return [
-    commands.registerCommand('copy-json-path.copy', () =>
+    commands.registerCommand(Copy.name, () =>
       new Copy(loggerService).register(),
     ),
   ];
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
 import { Disposable } from 'vscode';
-import { registerCommands } from './commands/commands';
+import { registerCommands } from './commands/commands.index';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposables: Disposable[] = registerCommands();
-
   context.subscriptions.push(...disposables);
 }
 
