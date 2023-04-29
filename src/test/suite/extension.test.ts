e3080a14954f1ad6e5047d597c6c9cdc266f03ae
Malo Polese <malo.polese@gmail.com>
2023-04-29 20:19:40 +0200

    feat(#18): add tests in CI
    Signed-off-by: Malo Polese <malo.polese@gmail.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,6 +3,7 @@ import getJsonPath, {
   propertyRequiresQuotes,
   getPropertyPathWithQuotes,
   nonQuotedCharacterRanges,
+  Options,
 } from '../../json.path';
 
 // You can import and use all API from the 'vscode' module
@@ -40,14 +41,22 @@ suite('Extension Test Suite', () => {
 
   test('getJsonPath test characters without escaping', () => {
     for (let c of charactersWithoutEscaping) {
-      test_getJsonPath(c, false);
+      let json = { [c]: 1 };
+      test_json_path(json, 3, c, 1);
     }
   });
 
   test('getJsonPath test characters that require escaping', () => {
-    test_getJsonPath('', true);
     for (let c of charactersWithEscapeRequired) {
-      test_getJsonPath(`_${c}_`, true);
+      let json = { [c]: 1 };
+      let expectedPath = `["${c}"]`;
+      if (c === `"`) {
+        expectedPath = `["\\""]`;
+      }
+      if (c === `\\`) {
+        expectedPath = `["\\\\"]`;
+      }
+      test_json_path(json, 3, expectedPath, 1);
     }
   });
 
@@ -60,73 +69,35 @@ suite('Extension Test Suite', () => {
       ) {
         const c = String.fromCharCode(index);
         console.log(`Testing range '${range}' char: '${c}', index: ${index}`);
-        test_getJsonPath(c, false);
+
+        let json = { [c]: 1 };
+        test_json_path(json, 3, c, 1);
       }
     }
   });
 
-  function test_getJsonPath(
-    propertyName: string,
-    expectQuoted: boolean,
-    propertyValue: any = 1,
-  ) {
-    const propertyNameEscaped = getPropertyPathWithQuotes(propertyName);
-    const expectedPropertyPath = expectQuoted
-      ? propertyNameEscaped
-      : '.' + propertyName;
-    test_getJsonPath_expected_path(
-      propertyName,
-      expectedPropertyPath,
-      propertyValue,
-    );
-  }
+  test('getJsonPath test change quote type', () => {
+    let json = { '+': 1 };
+    test_json_path(json, 3, `['+']`, 1, { quote: 'Single' });
+    test_json_path(json, 3, `["+"]`, 1, { quote: 'Double' });
+  });
 
-  function test_getJsonPath_expected_path(
-    propertyName: string,
-    expectedPropertyPath: string,
-    propertyValue: any = 1,
+  function test_json_path(
+    json: { [key: string]: any },
+    keyOffset: number,
+    expectedPath: string,
+    expectPropertyValue: any,
+    options?: Options,
   ) {
-    const jsonText = generateJson(propertyName, propertyValue);
-    try {
-      JSON.parse(jsonText);
-    } catch (exception) {
-      throw new Error(`Invalid generated json ${jsonText} \r ${exception}`);
+    const textJson = JSON.stringify(json);
+    let jsonPath = getJsonPath(textJson, keyOffset, undefined, options);
+
+    assert.strictEqual(jsonPath, expectedPath);
+
+    if (jsonPath[0] !== '[') {
+      jsonPath = '.' + jsonPath;
     }
-    const propertyCharPosition = 3;
-    test_getJsonPath_name(jsonText, propertyCharPosition, expectedPropertyPath);
-    test_getJsonPath_value(jsonText, propertyCharPosition, propertyValue);
-  }
-
-  function generateJson(propertyName: string, propertyValue: any = 1) {
-    var propertyNameEscaped = JSON.stringify(propertyName);
-    return `{ ${propertyNameEscaped}: ${propertyValue} }`;
-  }
-
-  function test_getJsonPath_name(
-    jsonText: string,
-    offsetPosition: number,
-    expectedPropertyPath: any,
-  ) {
-    var jsonPath = getJsonPath(jsonText, offsetPosition, undefined);
-    assert.strictEqual(
-      jsonPath,
-      expectedPropertyPath,
-      `jsonPath: ${jsonPath} expected to be ${expectedPropertyPath} from json: ${offsetPosition} ${jsonText}`,
-    );
-  }
-
-  function test_getJsonPath_value(
-    jsonText: string,
-    offsetPosition: number,
-    expectedValue: any,
-  ) {
-    var jsonPath = getJsonPath(jsonText, offsetPosition, undefined);
-    var parsedJson = JSON.parse(jsonText);
-    const actualValue = eval('parsedJson' + jsonPath);
-    assert.strictEqual(
-      actualValue,
-      expectedValue,
-      `Value retrieved was '${actualValue}' instead of '${expectedValue}' in json ${offsetPosition} ${jsonText}.`,
-    );
+    const actualPropertyValue = eval('json' + jsonPath);
+    assert.strictEqual(actualPropertyValue, expectPropertyValue);
   }
 });
